5fa543e2d91a61ef4aaedf439a90ff4b4d127586
neoskyclad <neoskyclad@naver.com>
2024-01-15 21:13:38 +0900

    [feat] 캐릭터 이동, 달리기, 웅크리기 구현

--- Source/SideRooms/Character/public/SideRoomsCharacter.h
+++ Source/SideRooms/Character/public/SideRoomsCharacter.h
@@ -14,7 +14,7 @@ class UCameraComponent;
 class UAnimMontage;
 class USoundBase;
 
-UCLASS(config=Game)
+UCLASS(Blueprintable, config=Game)
 class ASideRoomsCharacter : public ACharacter
 {
 	GENERATED_BODY()
@@ -24,11 +24,12 @@ public:
 
 protected:
 	virtual void BeginPlay();
+	virtual void Tick(float DeltaSeconds) override;
 
 //======== Component ============================
 public:
 	/** Pawn mesh: 1st person view (arms; seen only by self) */
-	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
 		USkeletalMeshComponent* Mesh1P;
 
 	/** First person camera */
@@ -53,6 +54,14 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 		class UInputAction* LookAction;
 
+	/** Sprint Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+		class UInputAction* SprintAction;
+
+	/** Crouch Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+		class UInputAction* CrouchAction;
+
 protected:
 	/** Called for movement input */
 	void Move(const FInputActionValue& Value);
@@ -60,11 +69,49 @@ protected:
 	/** Called for looking input */
 	void Look(const FInputActionValue& Value);
 
+	/** Called for starting sprint input */
+	void Sprint(const FInputActionValue& Value);
+
+	/** Called for sprinting input */
+	void Sprinting(const FInputActionValue& Value);
+
+	/** Called for stop sprinting input */
+	void StopSprinting(const FInputActionValue& Value);
+
+	/** Called for crouching input */
+	void Crouch(const FInputActionValue& Value);
+
+	/** Called for stop sprinting input */
+	void UnCrouch(const FInputActionValue& Value);
+
 protected:
 	// APawn interface
 	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
 	// End of APawn interface
 
+//======= Stats ====================================
+public:
+	/** Character HP **/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+		int32 CharacterHP;
+
+	/** Character Stamina **/
+	UPROPERTY(VisibleAnyWhere, BlueprintReadWrite, Category = Stats)
+		float CharacterStamina;
+
+	/** Character Sprint speed */
+	UPROPERTY(EditAnywhere, Category = Stats)
+		float SprintMultipleVal;
+
+private:
+	/** Flag for Stamina Control */
+	int StaminaFlag;
+
+	/**Stamina Recovery Value */
+	float StaminaIncreaseval;
+
+	/**Stamina Consuming Value */
+	float StaminaReduceVal;
 
 //======= Pickup ====================================
 public:
